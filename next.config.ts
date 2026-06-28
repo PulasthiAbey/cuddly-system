d8017406773b731a8130cceb05eca20052b82272
PA3699 <pulasthi.aberathne@zebra.com>
2026-06-29 00:06:34 +0530

    HMX:HMX-XXXX:ADD: Initialize project structure with Next.js and Tailwind CSS setup

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // Disables PWA in dev mode so it doesn't cache heavily
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+};
+
+module.exports = withPWA(nextConfig);
